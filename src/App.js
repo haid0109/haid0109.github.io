946490323d624be3f487232cc6057a06e273fe03
Haidar <Haidar@deranged.dk>
2022-01-12 22:37:22 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import venusIcon from './gender-female.svg';
+import marsIcon from './gender-male.svg';
+import plusIcon from './plus.svg';
+import minusIcon from './minus.svg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="bmi-calculator">
+        <h1 className="title">BMI Calculator</h1>
+        <div className="male-and-female-container">
+          <div id="male" className="container">
+            <img src={marsIcon} className="gender-icon" alt="logo" />
+            <span>MALE</span>
+          </div>
+          <div id="female" className="container">
+            <img src={venusIcon} className="gender-icon" alt="logo" />
+            <span>FEMALE</span>
+          </div>
+        </div>
+        <div id="height" className="container">
+          <span>HEIGHT</span>
+          <span>
+            <span className="stat-number">170</span>cm
+          </span>
+          <input type="range" min="120" max="220" defaultValue="170" class="height-slider"/>
+        </div>
+        <div className="weight-and-age-container">
+          <div id="weight" className="container">
+            <span>WEIGHT</span>
+            <span className="stat-number">60</span>
+            <div>
+              <button className="rounded-icon-button">
+                <img src={minusIcon} alt="logo" />
+              </button>
+              <button className="rounded-icon-button">
+                <img src={plusIcon} alt="logo" />
+              </button>
+            </div>
+          </div>
+          <div id="age" className="container">
+            <span>AGE</span>
+            <span className="stat-number">28</span>
+            <div>
+              <button className="rounded-icon-button">
+                <img src={minusIcon} alt="logo" />
+              </button>
+              <button className="rounded-icon-button">
+                <img src={plusIcon} alt="logo" />
+              </button>
+            </div>
+          </div>
+        </div>
+        <button id="calculateButton" className="calculate-button">CALCULATE</button>
+      </div>
     </div>
   );
 }
