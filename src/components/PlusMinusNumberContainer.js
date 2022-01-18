de7c0fe580cbbe6d932624154f2bbe71b7b652a2
Haidar <Haidar@deranged.dk>
2022-01-18 20:31:44 +0100

    styled plus minus number container with emotion and made more readable

--- src/components/PlusMinusNumberContainer.js
+++ src/components/PlusMinusNumberContainer.js
@@ -1,20 +1,36 @@
+import styled from '@emotion/styled';
 import plusIcon from '../images/plus.svg';
 import minusIcon from '../images/minus.svg';
+import Container from './Container';
+import Label from './Label';
+import StatNumber from './StatNumber';
 
 function PlusMinusNumberContainer(props) {
+  const RoundedIconButton = styled.button`
+    background-color: #4C4F5E;
+    border: none;
+    border-radius: 100%;
+    margin: .5em;
+    cursor: pointer;
+  `;
+
+  const Icon = styled.img`
+    height: 4em;
+  `;
+
   return (
-    <div className="container">
-      <span>{props.label}</span>
-      <span className="stat-number">{props.state.value}</span>
+    <Container>
+      <Label>{props.label}</Label>
+      <StatNumber>{props.value}</StatNumber>
       <div>
-        <button className="rounded-icon-button" onClick={() => props.state.setState(props.state.value - 1)}>
-          <img src={minusIcon} alt="logo" />
-        </button>
-        <button className="rounded-icon-button" onClick={() => props.state.setState(props.state.value + 1)}>
-          <img src={plusIcon} alt="logo" />
-        </button>
+        <RoundedIconButton onClick={() => props.changeValue(props.value - 1)}>
+          <Icon src={minusIcon} alt='minus-icon' />
+        </RoundedIconButton>
+        <RoundedIconButton onClick={() => props.changeValue(props.value + 1)}>
+          <Icon src={plusIcon} alt='plus-icon' />
+        </RoundedIconButton>
       </div>
-    </div>
+    </Container>
   );
 }
 
